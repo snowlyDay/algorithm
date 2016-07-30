bf0729925e1e5d43bff8e14fb6e09cbd19f2f865
snow <18753377106@163.com>
2016-07-30 12:44:03 +0800

    modified:   first.cpp

--- first.cpp
+++ first.cpp
@@ -1,8 +1,9 @@
+
 #include "stdio.h"
 #define LENGTH 100
 typedef char datatype;
 
-typedef struct node{
+typedef  struct node{
     datatype data;
     int lchild , rchild;
     int parent;
@@ -19,6 +20,40 @@ typedef struct BinNode{
 }BinNode;
 
 typedef BinNode* bintree;
+
+//  preorder about a tree
+void preorder(bintree t){
+    if(t){
+       printf(" %c\n",t->data);
+       preorder(bintree t);
+       preorder(bintree t);
+    }
+}
+
+
+
+// use the usual way to travel
+// to do a seqstack to  store  data
+#define SIZE 100
+
+typedef struct seqstack{
+     bintree data[SIZE];
+     int tag[SIZE];
+     int top ;
+}seqstack;
+
+void push (seqstack *s ,bintree t){
+
+      if (s->top ==SIZE) {
+        printf("the statck is full\n", );
+      }else{
+        s->top++;
+        s->data[s->top] =t;
+      }
+}
+
+
+
 
 
 void  mian() {
