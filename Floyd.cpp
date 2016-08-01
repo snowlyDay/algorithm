6cca784feb892e9b689c732825141bcf4377de44
snowlyday <snowlydayMail@163.com>
2016-08-01 08:57:56 +0800

    new file:   Floyd.cpp   hdoj 3665 get the shortest way to find city

---
+++ Floyd.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+using namespace std;
+const int Max=105;
+const int INF =9999;
+int n, map[Max][Max],d[Max],vis[Max],issea[Max];
+int int main(int argc, char const *argv[]) {
+  int i ,j mine ,mi ,pi, si,li,k;
+  while (scanf("%d\n", &n)!=EOF ) {
+    mine =INF;
+    for (int i = 0; i < n ; i++) {
+        for (int  j = 0; j < n; j++) {
+          map[i][j]=INF;
+        }
+    }
+    for (int  i = 0; i <n; i++) {
+      scanf("%d %d \n",,&mi,&issea[i]);
+      
+
+    }
+  }
+  return 0;
+}
